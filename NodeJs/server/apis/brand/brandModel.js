93067e4614c5537f2c724c4fad1cdbea381e81da
Purva-Pathania <pathaniapurva40@gmail.com>
2025-04-22 14:53:37 +0530

    Delete NodeJs directory

--- NodeJs/server/apis/brand/brandModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-let brandSchema = mongoose.Schema({
-    autoId:{type:Number, default:1},
-    brandName:{type:String, default:""},
-    image:{type:String, default:""},
-    status:{type:Boolean, default:true},
-    createdAt:{type:Date, default:Date.now()}
-})
-module.exports=mongoose.model("brandModel", brandSchema)
